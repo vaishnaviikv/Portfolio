7aa59c49c1fc40bb55a69d1867605c83d499bed0
Vaishnavi V <35933479+vaishnaviikv@users.noreply.github.com>
2023-09-29 16:53:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript functionality here if needed
+document.getElementById('download').addEventListener('click', function() {
+    // Add logic to trigger the download of the resume/CV
+    alert('Download functionality coming soon!');
+});
